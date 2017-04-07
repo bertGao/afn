80dc7625b78bf69408a90025730e00758f246b3f
bert <bert@bertdeMac-mini.local>
2017-04-07 17:44:16 +0800

    12312321

--- RootViewController.h
+++ RootViewController.h
@@ -1,11 +1,4 @@
-////
-////  RootViewController.h
-////  af
-////
-////  Created by dllo on 15/4/17.
-////  Copyright (c) 2015年 dllo. All rights reserved.
-////
-//
+
 //#import <UIKit/UIKit.h>
 //
 //@interface RootViewController : UIViewController
